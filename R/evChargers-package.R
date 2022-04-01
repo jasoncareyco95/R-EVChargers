4e6a32e4cd37a7a0bb1b31f788cb62211c6aac25
Jason Carey <jasoncareyco95@outlook.com>
2022-04-01 10:43:12 -0600

    Package updates

---
+++ R/evChargers-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+utils::globalVariables(
+  c(
+    ".", ".x", "L1", "all_roads", "dist", "distance", "duration", "end", "geometry", "index", "lead", "level", "rn", "start"
+  )
+)
